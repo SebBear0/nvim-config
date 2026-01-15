f82d9fbd99ef4145e4489f460cf92ad56351c25d
Seb Bearsley <sebastian.bearsley@gmail.com>
2026-01-15 20:18:13 +1100

    added typst lsp

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -213,6 +213,7 @@ return {
         -- pyright = {},
         -- rust_analyzer = {},
         nil_ls = {},
+        tinymist = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
