4adfcd084e2c505a6afae1d4b0157d26a3433e3b
Seb Bearsley <sebastian.bearsley@gmail.com>
2026-01-22 10:59:38 +1100

    adjusted treesitter config

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -5,11 +5,11 @@ return {
 		main = 'nvim-treesitter.config', -- Sets main module to use for opts
 		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 		opts = {
-			ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'rust', 'typst' },
+			ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'typst' },
 			-- Autoinstall languages that are not installed
 			auto_install = false,
 			highlight = {
-				enable = true,
+				enable = false,
 				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
 				--  If you are experiencing weird indenting issues, add the language to
 				--  the list of additional_vim_regex_highlighting and disabled languages for indent.
